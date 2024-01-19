1bbe20d13321d3661c169d86b869b43841cceb8f
TUGUIHUA <907678455@qq.com>
2024-01-19 17:14:03 +0800

    env配置修改，新增VITE_APP_BASE_Backend_Url 后端接口访问url

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,15 @@ import createVitePlugins from './vite/plugins'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode, command }) => {
+ 
   const env = loadEnv(mode, process.cwd())
-  const { VITE_APP_ENV } = env
+  const envAll = loadEnv(mode, process.cwd(),'') 
   return {
     // 部署生产环境和开发环境下的URL。
     // 默认情况下，vite 会假设你的应用是被部署在一个域名的根路径上
     // 例如 https://www.scada.vip/。如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。例如，如果你的应用被部署在 https://www.scada.vip/admin/，则设置 baseUrl 为 /admin/。
-    base: VITE_APP_ENV === 'production' ? '/' : '/',
-    plugins: createVitePlugins(env, command === 'build'),
+    base: envAll.VITE_APP_ENV === 'production' ? '/' : '/',
+    plugins: createVitePlugins(envAll, command === 'build'),
     resolve: {
       // https://cn.vitejs.dev/config/#resolve-alias
       alias: {
@@ -30,10 +31,8 @@ export default defineConfig(({ mode, command }) => {
       // port: 8012, 
       proxy: {
         // https://cn.vitejs.dev/config/#server-proxy
-        '/api': {
-          target: 'https://127.0.0.1:7062',//本地测试
-          // target: 'https://192.168.36.195:5000',//Linux服务器
-          // target: 'https://192.168.0.101:5000',//天津铁厂192.168.0.101服务器
+        [envAll.VITE_APP_BASE_API]: {
+          target: envAll.VITE_APP_BASE_Backend_Url , 
           
           ws: true,        //如果要代理 websockets，配置这个参数
           changeOrigin: true,  //是否跨域
