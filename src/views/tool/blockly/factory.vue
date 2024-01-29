a30a098e6510398c4ba8a71e9c3b94a015751f2d
TUGUIHUA <907678455@qq.com>
2024-01-30 01:10:17 +0800

    public 下添加blocklyfactory

--- src/views/tool/blockly/factory.vue
+++ src/views/tool/blockly/factory.vue
@@ -1,13 +1,31 @@
 <template>
+    <div>
+        <iframe src="/blockly/blockfactory/index.html" id="mobsf" frameborder="0"
+            style="position:absolute;"></iframe>
+   
+    </div>
+</template>
  
-    <i-frame v-model:src="url"></i-frame>
+<script setup>
 
- </template>
- 
- <script setup>
- import iFrame from '@/components/iFrame'
- 
- const url = ref("../../../blockfactory/index.html") 
- console.log(url)
- </script>
+
+/**
+* iframe-宽高自适应显示   
+*/
+function changeMobsfIframe() {
+    const mobsf = document.getElementById('mobsf');
+    const deviceWidth = document.body.clientWidth;
+    const deviceHeight = document.body.clientHeight;
+    mobsf.style.width = (Number(deviceWidth) ) + 'px'; //数字是页面布局宽度差值
+    mobsf.style.height = (Number(deviceHeight)) + 'px'; //数字是页面布局高度差
+}
+
+onMounted(() => {
+    changeMobsfIframe()
+
+    window.onresize = function () {
+        changeMobsfIframe()
+    }
+})
+</script>
  
